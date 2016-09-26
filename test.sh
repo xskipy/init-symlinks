e35bbe2e55ff2289537d385b94fc6d64ce587091
Daniel Maixner <xskipy@gmail.com>
2016-09-26 12:34:03 +0200

    Fixed doubling values and locating files. log file formatting is also fixed
    --amend
    
    --amend
    
    --amend

--- test.sh
+++ test.sh
@@ -1,53 +1,79 @@
 #!/usr/bin/bash
-#set -x
+#
+#Created by Daniel Maixner <xskipy@gmail.com>
+#  	Under GNU GENERAL PUBLIC LICENSE
+#
+#
+#
+#
 cd /etc/rc.d/init.d
 touch /home/$USER/testlog.txt
 echo "
-" >> /home/$USER/testlog.txt
+
+---------------------------------------------------------" >> /home/$USER/testlog.txt
+
 date >> /home/$USER/testlog.txt
 for INITNAME in $(ls --ignore "functions" --ignore "README")
 do
-	#printf "Init %s : \n" "$INITNAME"	#COLOR THIS LINE
+	printf "Testing init %s : \n" "$INITNAME"	
 	chkconfigSettings=$(grep "chkconfig" /etc/rc.d/init.d/"$INITNAME")
-	#printf "%s \n" "$chkconfigSettings" 
 	RUNLEVELS=$(echo "$chkconfigSettings" | awk '{printf $3}') 	#RUNLEVEL VARIABLE INITIALIZATION..
+	printf "On run levels.. %s \n" "$RUNLEVELS" 
 	STA=$(echo "$chkconfigSettings" | awk '{printf $4}')
 	END=$(echo "$chkconfigSettings" | awk '{printf $5}')
 	#printf "%s" $RUNLEVELS
+	ok=true
+	first=0
 	if [ ! $RUNLEVELS == "-" ]; then 
 		for i in $(seq 0 1 ${#RUNLEVELS}); do		# I - position in string RUNLEVELS
 			n=${RUNLEVELS:$i:1}
-			printf "%s " $n
-			printf "%s " $RUNLEVELS
-			printf "%s " $INITNAME
-			printf "%s \n" $i	
-			set -x
-			for control in $(seq 0 1 6); do
-				if [ "$control" -eq $n ]; then
-					if [ -e /etc/rc.d/rc$control.d/S$STA$INITNAME ]; then
-						echo "S$STA$INITNAME found in /etc/rc.d/rc$control.d/" >> /home/$USER/testlog.txt
+			if [ ! "$n" == '' ]; then 
+				for lev in $(seq $first 1 6); do 
+					if [ $lev == $n ]; then
+						if [ -e /etc/rc.d/rc$n.d/S$STA$INITNAME ]; then
+							echo "S$STA$INITNAME found in /etc/rc.d/rc$lev.d/" >> /home/$USER/testlog.txt
+							first=$(($n+1))
+							continue 2
+				
+						else
+							echo "S$STA$INITNAME NOT FOUND!!! in /etc/rc.d/rc$lev.d/"  >> /home/$USER/testlog.txt
+							first=$(($n+1))
+							ok=false
+							continue 2
+						fi
 					else
-						echo "S$STA$INITNAME NOT FOUND!!! in /etc/rc.d/rc$control.d/"  >> /home/$USER/testlog.txt
+						if [ -e /etc/rc.d/rc$lev.d/K$END$INITNAME ]; then
+							echo "K$END$INITNAME found in /etc/rc.d/rc$lev.d/" >> /home/$USER/testlog.txt
+						else
+							echo "K$END$INITNAME NOT FOUND!!! in /etc/rc.d/rc$lev.d/"  >> /home/$USER/testlog.txt
+							ok=false
+						fi
 					fi
-				else
-					if [ -e /etc/rc.d/rc$control.d/K$END$INITNAME ]; then
-						echo "K$END$INITNAME found in /etc/rc.d/rc$control.d/" >> /home/$USER/testlog.txt
-					else
-						echo "K$END$INITNAME NOT FOUND!!! in /etc/rc.d/rc$control.d/" >> /home/$USER/testlog.txt
-					fi
-				fi
-			#echo "/etc/rc.d/rc$n.d/S$STA$INITNAME" >> /home/$USER/testlog.txt
-			done
+				done
+			fi
 		done
 	else
 		for k in $(seq 0 1 6); do
 			if [ -e /etc/rc.d/rc$k.d/K$END$INITNAME ]; then
 				echo "K$END$INITNAME found in /etc/rc.d/rc$k.d/" >> /home/$USER/testlog.txt
 			else
-				echo "K$END$INITNAME NOT FOUND!!! in /etc/rc.d/rc$ck.d/" >> /home/$USER/testlog.txt
+				echo "K$END$INITNAME NOT FOUND!!! in /etc/rc.d/rc$k.d/"  >> /home/$USER/testlog.txt
+				ok=false
 			fi
-		done
+		done	
 	fi
 	#printf "%s \n" "$chkconfigSettings"
 	#printf "\n"
+
+	if [ $ok = true ]; then 
+				printf "Init $INITNAME is ok." >>  /home/$USER/testlog.txt
+				echo "Ok.."
+
+		else 
+				printf "Init $INITINAME is NOT OK, see log^." >> /home/$USER/testlog.txt
+				echo "Fail.."
+	fi
+	echo "
+" >> /home/$USER/testlog.txt
 done
+echo "Log file created at.. /home/$USER/testlog.txt"
